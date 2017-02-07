f2c70c563a7e761fd6645002a8ae1c4261b15dc9
Carlos <cstaszeski@gmail.com>
2017-02-07 17:00:49 -0500

    Empty repo

--- xorly.py
+++
@@ -1,9 +0,0 @@
-def encrypt(plaintext, key):
-
-    ciphertext = []
-    for i in range(0, len(plaintext)):
-        ciphertext.append(ord(plaintext[i]) ^ ord(key[i%len(key)])) 
-
-    return ''.join(map(chr, ciphertext))
-
-decrypt = encrypt
