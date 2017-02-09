7a593a892b418023aca8e4c6fb8e8942dbd7a331
Carlos <cstaszeski@gmail.com>
2017-02-09 16:43:34 -0500

    Got rid of comments to make code cleaner

--- breaksinglebytexor.py
+++ breaksinglebytexor.py
@@ -81,5 +81,5 @@ def breakSingleByteXor(s):
 crypt = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 
 ciphertext = breakSingleByteXor(binascii.unhexlify(crypt))
-#breakSingleByteXor(binascii.unhexlify(crypt))
+
 print(ciphertext[-1][0])
